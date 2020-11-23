84a02b983b273e54641814286ed6410ca31a9142
Adeola-Oni <oniadeola33@gmail.com>
2020-11-23 17:30:30 +0100

    error boundaries have been added

---
+++ src/Carousel.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: [],
+      active: 0,
+    };
+
+    // this.handleIndexClick = this.handleIndexClick.bind;
+    //or arrow function;
+  }
+  //   state = { photos: [], active: 0 };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+    return { photos };
+  }
+
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+    console.log(e.target.dataset.index);
+  };
+
+  render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="" animal></img>
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            //eslint-disable-next-line
+            <img
+              key={photo.large}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : 0}
+              alt="animal-thumbnail"
+            ></img>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
